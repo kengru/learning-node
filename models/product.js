434423f3b0fbc4b20740da370586f994d27ec28f
kengru <kengrullon@gmail.com>
2019-03-30 00:12:41 -0400

    Retrieving and saving the product data from a file.

--- models/product.js
+++ models/product.js
@@ -1,4 +1,21 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (error, data) => {
+    if (error) {
+      cb([]);
+    } else {
+      cb(JSON.parse(data));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +23,15 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), error => {
+        console.log(error);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
