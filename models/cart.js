4e48d6de58cc38cba172be57ee7f631f31851473
Kendry Grullon <kengrullon@gmail.com>
2019-04-01 11:28:19 -0400

    Added a static method on the cart model to save products.

--- models/cart.js
+++ models/cart.js
@@ -1,7 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
 module.exports = class Cart {
-  static AddProduct(id) {
+  static addProduct(id, productPrice) {
     // fetch previous cart
-    // analyze the cart
-    // add new product.
+    fs.readFile(p, (error, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!error) {
+        cart = JSON.parse(fileContent);
+      }
+      // analyze the cart
+      const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [ ...cart.products, updatedProduct ]
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), error => {
+        console.log(error);
+      })
+    });
   }
-}
+};
