972e3874c7eed3ca4913523d0acb51e7a3d46feb
Kendry Grullon <kengrullon@gmail.com>
2019-04-03 09:46:05 -0400

    Creating a user model and associating it with products.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 3000;
 
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const ProductModel = require("./models/product");
+const UserModel = require("./models/user");
 
 const app = express();
 
@@ -18,14 +20,37 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  UserModel.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(error => console.log(error));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+ProductModel.belongsTo(UserModel, { constraints: true, onDelete: "CASCADE" });
+UserModel.hasMany(ProductModel);
+
 sequelize
+  // .sync({force: true})
   .sync()
   .then(result => {
+    return UserModel.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      UserModel.create({ name: "Ken", email: "ken@gru.com" });
+    }
+    return Promise.resolve(user);
+  })
+  .then(user => {
+    console.log(user);
     console.log("Listening at port:", PORT);
     app.listen(PORT);
   })
