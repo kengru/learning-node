d30abc1b423d298c77bfd248c27dbb8558dd2502
kengru <kengrullon@gmail.com>
2019-04-23 19:52:19 -0400

    Getting invoices from data folder.

--- routes/shop.js
+++ routes/shop.js
@@ -18,6 +18,7 @@ router.post("/cart-delete-item", isAuth, shopController.postCartDelete);
 router.get("/orders", isAuth, shopController.getOrders);
 router.post("/create-order", isAuth, shopController.postOrder);
 
+router.get("/orders/:orderId", isAuth, shopController.getInvoice)
 // router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
