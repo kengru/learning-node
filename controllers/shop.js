20bdf8c364c3fe7447e80c4d85cdd8b2aa94ab81
kengru <kengrullon@gmail.com>
2019-04-23 23:12:33 -0400

    Creating pdfs with pdfkit.

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const PDFDocument = require("pdfkit");
+
 const Product = require("../models/product");
 const Order = require("../models/order");
 
@@ -96,16 +98,45 @@ exports.postOrder = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const { orderId } = req.params;
-  const invoiceName = `invoice-${orderId}.pdf`;
-  const invoicePath = path.join("data", "invoices", invoiceName);
-  fs.readFile(invoicePath, (err, data) => {
-    if (err) {
-      return next(err);
-    }
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", `inline; filename='${invoiceName}'`);
-    res.send(data);
-  });
+  Order.findById(orderId)
+    .then(order => {
+      if (!order) {
+        return next(new Error("No order found."));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Unauthorized"));
+      }
+      const invoiceName = `invoice-${orderId}.pdf`;
+      const invoicePath = path.join("data", "invoices", invoiceName);
+
+      const pdfDoc = new PDFDocument();
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.text("Hello!");
+
+      pdfDoc.end();
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) {
+      //     return next(err);
+      //   }
+      //   res.setHeader("Content-Type", "application/pdf");
+      //   res.setHeader(
+      //     "Content-Disposition",
+      //     `inline; filename='${invoiceName}'`
+      //   );
+      //   res.send(data);
+      // fs.stat(invoicePath, (err, stat) => {
+      //   if (err) {
+      //     return next(new Error("Unable to stream file."));
+      //   }
+      // })
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader("Content-Disposition", `inline; filename='${invoiceName}'`);
+      file.pipe(res);
+    })
+    .catch(err => next(err));
 };
 
 exports.getCheckout = (req, res, next) => {
