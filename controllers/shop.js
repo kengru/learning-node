8a314ad6926757ab25b65c485ccb706135dd680d
kengru <kengrullon@gmail.com>
2019-04-23 23:20:30 -0400

    Adding invoice info with prices on the pdf.

--- controllers/shop.js
+++ controllers/shop.js
@@ -99,6 +99,7 @@ exports.postOrder = (req, res, next) => {
 exports.getInvoice = (req, res, next) => {
   const { orderId } = req.params;
   Order.findById(orderId)
+    .populate("items.product")
     .then(order => {
       if (!order) {
         return next(new Error("No order found."));
@@ -113,7 +114,21 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
 
-      pdfDoc.text("Hello!");
+      pdfDoc.fontSize(26).text("Invoice", { underline: true });
+      pdfDoc.text("----------------------");
+
+      let totalPrice = 0;
+      order.items.forEach(prod => {
+        pdfDoc
+          .fontSize(14)
+          .text(
+            `${prod.product.title} - ${prod.quantity} x $${prod.product.price}`
+          );
+        totalPrice += prod.quantity * prod.product.price;
+      });
+
+      pdfDoc.text("-------");
+      pdfDoc.fontSize(20).text(`Total Price: $${totalPrice}`);
 
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
